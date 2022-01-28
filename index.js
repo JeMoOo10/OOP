8c7c39b1f161974f42c731a07cfda929ae24c114
Jamal Albatta <jemooo91@gmail.com>
2022-01-28 13:22:35 +0100

    Taak 7

--- index.js
+++ index.js
@@ -2,31 +2,76 @@
 class GoedDoel { 
     #opbrengst; 
     #naam;
-    constructor(naam){
+    #adres;
+    #contactpersonen;
+    constructor(naam,adres){
         this.#opbrengst = 0;
         this.#naam = naam;
-    }
+        this.#adres = adres;
+        this.#contactpersonen = [];
+    };
     stort(bedrag) { 
         if (bedrag > 0) { 
             this.#opbrengst += bedrag; 
-        }
-    } 
+        };
+    };
     getOpbrengst() { 
         return this.#opbrengst; 
-    }
+    };
 
     getNaam(){
         return this.#naam;
-    }
+    };
+
+    getAdres() {
+        return this.#adres;
+    };
+
+    voegContactpersoonToe(persoon) { 
+        this.#contactpersonen.push(persoon); 
+    };
+    getContactpersonen() {
+        return this.#contactpersonen;
+    };
+};
+
+class Adres {
+    #straat;
+    #huisnummer;
+    #postcode;
+    #gemeente;
+    constructor(straat, huisnummer, postcode, gemeente) {
+        this.#straat = straat;
+        this.#huisnummer = huisnummer;
+        this.#postcode = postcode;
+        this.#gemeente = gemeente;
+    };
+    getVolledigAdres() {
+        return `${this.#straat} ${this.#huisnummer} ${this.#postcode} ${this.#gemeente}`;
+    };
+};
+
+
+class Persoon {
+    #voornaam;
+    #familienaam;
+    constructor(voornaam, familienaam) {
+        this.#voornaam = voornaam;
+        this.#familienaam = familienaam;
+    };
+    getNaam() {
+        return `${this.#voornaam} ${this.#familienaam}`;    
+    };
+};
+
+
+const cliniClowns = new GoedDoel("clinik", new Adres("schipstraat","6","8530","Harelbeke"));
+console.log(cliniClowns.getAdres().getVolledigAdres())
+
+
+cliniClowns.voegContactpersoonToe(new Persoon("Arthur", "Janssens")); 
+cliniClowns.voegContactpersoonToe(new Persoon("Emma", "Peeters"));
+console.log(cliniClowns)
+for (const persoon of cliniClowns.getContactpersonen()) { 
+console.log(persoon.getNaam()); 
 }
-
-
-const goedeDoelen = [ 
-    new GoedDoel("Kom op tegen kanker"), 
-    new GoedDoel("Cliniclowns") 
-    ];
-
-
-    for (const goedDoel of goedeDoelen) { 
-        console.log(goedDoel.getNaam()); 
-        };
--- taken.js
+++ taken.js
@@ -1,24 +1,46 @@
 "use strict"
 
-class Vierkant {
-    #zijde;
-    constructor(zijde){
-        this.#zijde = zijde;
-    };
+class LijnStuk{
+    #eindpunt1;
+    #eindpunt2;
+    constructor(eindpunt1,eindpunt2){
+        this.#eindpunt1 = eindpunt1;
+        this.#eindpunt2 = eindpunt2;
+    }
 
-    getOmtrek(){
-        return this.#zijde * 4;
+    isHorizontaal(){
+        console.log("is het Horizontaal ?")
+        return this.#eindpunt1.getY() === this.#eindpunt2.getY(); 
+        
+    }
 
-    };
+    isVerticaal(){
+        console.log("is het Verticaal ?")
+        return this.#eindpunt1.getX() === this.#eindpunt2.getX();
+    }
+}
 
-    getOppervlakte(){
-        return this.#zijde * this.#zijde;
-    };
-};
 
-const vierkanten = [ new Vierkant(3), new Vierkant(5)];
+class Punt{
+    #x;
+    #y;
+    constructor(x,y){
+        this.#x = x;
+        this.#y = y;
+    }
 
-for(const vierkant of vierkanten){
-    console.log(vierkant.getOmtrek());
-    console.log(vierkant.getOppervlakte());
-};
+    getX(){
+        return this.#x;
+    }
+
+    getY(){
+        return this.#y;
+
+    }
+}
+
+
+const lijn1 = new LijnStuk (new Punt(8,6),new Punt(8,7)) 
+
+console.log(lijn1.isHorizontaal())
+console.log(lijn1.isVerticaal())
