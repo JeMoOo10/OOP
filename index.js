a5ec7919bbc723542bc59162e262ae5e2ed6cc72
Jamal Albatta <jemooo91@gmail.com>
2022-01-28 17:24:25 +0100

    Taak 11

--- index.js
+++ index.js
@@ -1,61 +1,16 @@
 "use strict";
-class Artikel {
-    #naam;   // 1
-    #prijsExclusiefBTW;
-    #btwPercentage;
-    constructor(naam, prijsExclusiefBTW, btwPercentage) {
+    class Land {
+    #naam; 
+    static #aantal = 0; 
+    constructor(naam) {
         this.#naam = naam;
-        this.#prijsExclusiefBTW = prijsExclusiefBTW;
-        this.#btwPercentage = btwPercentage;
+        Land.#aantal++; 
     }
-    getNaam() {
-        return this.#naam;
-    }
-    getPrijsExclusiefBTW() {
-        return this.#prijsExclusiefBTW;
-    }
-    getPrijsInclusiefBTW() {
-        return Number((this.#prijsExclusiefBTW * (1 + this.#btwPercentage / 100)).toFixed(2));
-    }
-
-    getInfo() {
-        return `${this.#naam}: € ${this.getPrijsInclusiefBTW()}`;
+    static getAantal() { 
+        return Land.#aantal;
     }
 }
-
-class FoodArtikel extends Artikel {    // 2
-    #vervalPeriode;   // 3
-        constructor(naam, prijsExclusiefBTW, btwPercentage, vervalPeriode) {
-        super(naam, prijsExclusiefBTW, btwPercentage);  // 4
-        this.#vervalPeriode = vervalPeriode;
-    }
-    getVervalPeriode() {  // 5
-        return this.#vervalPeriode;
-    }
-
-    getInfo() {
-        return `${super.getInfo()} vervalt na ${this.#vervalPeriode} dagen`; 
-    }
-}
-
-
-
-class NonFoodArtikel extends Artikel {
-    #garantiePeriode;
-    constructor(naam, prijsExclusiefBTW, btwPercentage, garantiePeriode) {
-        super(naam, prijsExclusiefBTW, btwPercentage);
-        this.#garantiePeriode = garantiePeriode;
-    }
-    getGarantiePeriode() {
-        return this.#garantiePeriode;
-    }
-}
-
-
-const artikels = [ 
-    new FoodArtikel("druif", 6, 6, 7), 
-    new NonFoodArtikel("papier", 10, 21, 1) 
-    ];
-    for (const artikel of artikels) { 
-    console.log(artikel.getInfo()); 
-    }
+console.log(Land.getAantal()); 
+const land = new Land("België");
+const buurland = new Land("Nederland");
+console.log(Land.getAantal());
--- taken.js
+++ taken.js
@@ -1,36 +1,90 @@
-"use strict"
-
-class Huisdier{
-    #naam;
-    constructor(naam){
-        this.#naam = naam;
-    }
-
-    getNaam(){
-        return this.#naam;
-    }
+"use strict";
+class Tegel {
+#prijs;
+constructor(prijs) {
+this.#prijs = prijs;
 }
-
-
-class Hond extends Huisdier{
-    
-    getGeluid(){
-        return "woof woof";
-    }
+getPrijs() {
+return this.#prijs;
 }
-
-
-class Cat extends Huisdier{
-
-    getGeluid(){
-        return "meow meow";
-    }
 }
 
 
 
-const dieren = [ new Hond("dog") , new Cat("cat")]
-
-for(const dier of dieren){
-    console.log(dier.getNaam(),dier.getGeluid())
+class VierkanteTegel extends Tegel {
+#zijde;
+constructor(prijs, zijde) {
+super(prijs);
+this.#zijde = zijde;
 }
+getOppervlakte() {
+return Number((this.#zijde * this.#zijde).toFixed(2));
+}
+}
+class RechthoekigeTegel extends Tegel {
+#lengte;
+#breedte;
+constructor(prijs, lengte, breedte) {
+super(prijs);
+this.#lengte = lengte;
+this.#breedte = breedte;
+}
+getOppervlakte() {
+return Number((this.#lengte * this.#breedte).toFixed(2));
+}
+}
+class RondeTegel extends Tegel {
+#straal;
+constructor(prijs, straal) {
+super(prijs);
+this.#straal = straal;
+}
+getOppervlakte() {
+return Number((this.#straal * this.#straal * Math.PI).toFixed(2));
+}
+}
+class BestelRegel {
+#tegel;
+#aantal;
+constructor(tegel, aantal) {
+this.#tegel = tegel;
+this.#aantal = aantal;
+}
+getOppervlakte() {
+return Number((this.#tegel.getOppervlakte() * this.#aantal).toFixed(2));
+}
+getTeBetalen() {
+return Number((this.#tegel.getPrijs() * this.#aantal).toFixed(2));
+}
+}
+
+
+
+class Bestelling {
+#bestelRegels = [];
+voegBestelRegelToe(bestelRegel) {
+this.#bestelRegels.push(bestelRegel);
+}
+getOppervlakte() {
+let oppervlakte = 0;
+for (const bestelRegel of this.#bestelRegels) {
+oppervlakte += bestelRegel.getOppervlakte();
+}
+return Number(oppervlakte.toFixed(2));
+}
+getTeBetalen() {
+let teBetalen = 0;
+for (const bestelRegel of this.#bestelRegels) {
+teBetalen += bestelRegel.getTeBetalen();
+}
+return Number(teBetalen.toFixed(2));
+}
+}
+
+
+
+const bestelling = new Bestelling();
+bestelling.voegBestelRegelToe(new BestelRegel(new VierkanteTegel(4, 3), 10));
+bestelling.voegBestelRegelToe(new BestelRegel(new RechthoekigeTegel(4,5,3),10));
+bestelling.voegBestelRegelToe(new BestelRegel(new RondeTegel(4, 3), 10));
+console.log(bestelling.getOppervlakte(), bestelling.getTeBetalen());
