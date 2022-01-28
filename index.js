77dae9d9a649c5d9f08feeb868274a4a70e3955c
Jamal Albatta <jemooo91@gmail.com>
2022-01-28 10:29:54 +0100

    taak 4

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 "use strict";
 class GoedDoel { 
-    #opbrengst = 0; 
+    #opbrengst; 
+    #naam;
+    constructor(naam){
+        this.#opbrengst = 0;
+        this.#naam = naam;
+    }
     stort(bedrag) { 
         if (bedrag > 0) { 
             this.#opbrengst += bedrag; 
@@ -9,16 +14,20 @@ class GoedDoel {
     getOpbrengst() { 
         return this.#opbrengst; 
     }
+
+    getNaam(){
+        return this.#naam;
+    }
 }
 
 
-const komOpTegenKanker = new GoedDoel(); 
+const komOpTegenKanker = new GoedDoel("Kom op tegen kanker");
 komOpTegenKanker.stort(100); 
 komOpTegenKanker.stort(200);
 console.log(komOpTegenKanker.getOpbrengst()); 
-const cliniClowns = new GoedDoel(); 
+const cliniClowns = new GoedDoel("cliniClowns"); 
 cliniClowns.stort(500);
 console.log(cliniClowns.getOpbrengst());
-console.log(komOpTegenKanker.stort(-6000));
-console.log(komOpTegenKanker.getOpbrengst()); 
+console.log(komOpTegenKanker.getNaam());
+console.log(cliniClowns.getNaam());
 
--- taken.js
+++ taken.js
@@ -1,37 +1,25 @@
 "use strict"
 
-class Bankrekening{
-    #saldo = 0;
-
-    stort(bedrag){
-        if(bedrag > 0){
-        this.#saldo += bedrag;
-        }
+class Rechthoek{
+    #lengte;
+    #breedte;
+    constructor(lengte,breedte){
+        this.#lengte = lengte;
+        this.#breedte = breedte;
     }
 
-    haalAf(bedrag){
-        if(bedrag > 0 && bedrag <= this.#saldo){
-            this.#saldo -= bedrag;
-        } else {
-            console.log( "not good")
-        }
+    getOmtrek(){
+        return (this.#lengte + this.#breedte) * 2;
     }
 
-    getSaldo(){
-        return this.#saldo;
+    getOppervlakte(){
+        return this.#breedte * this.#lengte;
     }
+
+    
 }
 
 
-const rekening = new Bankrekening();
-console.log(rekening.getSaldo());
-rekening.stort(10);
-console.log(rekening.getSaldo());
-rekening.stort(-2);
-console.log(rekening.getSaldo());
-rekening.haalAf(2);
-console.log(rekening.getSaldo());
-rekening.haalAf(-2);
-console.log(rekening.getSaldo());
-rekening.haalAf(20);
-console.log(rekening.getSaldo());
+const rechthoek = new Rechthoek(6, 4);
+console.log(rechthoek.getOmtrek());
+console.log(rechthoek.getOppervlakte());
